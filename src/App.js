f3e30ada2360606c2fcc1cb986ce2014a584f67b
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-15 21:15:26 +0530

    add material UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
 import './App.css';
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button variant="contained">
+          Click me!        
+        </Button>
         <a
           className="App-link"
           href="https://reactjs.org"
