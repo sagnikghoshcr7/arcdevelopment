6eea2d8ddbff0556c0ffc1ce6ecd20ea227a8c95
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-16 00:23:09 +0530

    add assets & animation files

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import { makeStyles } from '@material-ui/core/styles';
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -17,16 +17,25 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles(theme => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar
+  }
+}))
+
 export default function Header(props) {
+  const classes = useStyles()
+
   return (
-    <ElevationScroll>
-      <AppBar position='fixed'>
-        <Toolbar>
-          <Typography variant='h3'>
-            Arc Development
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <React.Fragment>
+      <ElevationScroll>
+        <AppBar position='fixed'>
+          <Toolbar>
+            
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </React.Fragment>    
   );
 }
