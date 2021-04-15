48fcdd9af57ad0ce2ee20cb64cf982589b035b92
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-15 23:59:01 +0530

    add theme color

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/styles';
+
+import theme from './ui/Theme';
 import Header from '../components/ui/Header';
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
       <Header />
-      {[...new Array(120)]
-        .map(
-          () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-        )
-        .join('\n')}
-    </div>
+      Hello!
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/ui/Theme.jsx
@@ -0,0 +1,19 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const arcBlue = '#0B72B9'
+const arcOrange = '#FFBA60'
+
+export default createMuiTheme({
+  palette: {
+    common: {
+      blue: `${arcBlue}`,
+      orange: `${arcOrange}`
+    },
+    primary: {
+      main: `${arcBlue}`
+    },
+    secondary: {
+      main: `${arcOrange}`
+    }
+  }  
+});
