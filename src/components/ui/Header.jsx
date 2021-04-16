29e3d45e079dfc26b8addf68a204742264001bfb
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-16 17:02:04 +0530

    refresh bug fix

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -111,6 +111,60 @@ export default function Header(props) {
     } else if (window.location.pathname === "/estimate" && value !== 5) {
       setValue(5);
     }
+
+    switch (window.location.pathname) {
+      case "/":
+        if (value !== 0) {
+          setValue(0)
+        }
+        break;
+      case "/services":
+        if (value !== 1) {
+          setValue(1)
+          setSelectedIndex(0)
+        }
+        break;
+      case "/customsoftware":
+        if (value !== 1) {
+          setValue(1)
+          setSelectedIndex(1)
+        }
+        break;
+      case "/mobileapps":
+        if (value !== 1) {
+          setValue(1)
+          setSelectedIndex(2)
+        }
+        break;
+      case "/websites":
+        if (value !== 1) {
+          setValue(1)
+          setSelectedIndex(3)
+        }
+        break;
+      case "/revolution":
+        if (value !== 2) {
+          setValue(2)
+        }
+        break;
+      case "/about":
+        if (value !== 3) {
+          setValue(3)
+        }
+        break;
+      case "/contact":
+        if (value !== 4) {
+          setValue(4)
+        }
+        break;
+      case "/estimate":
+        if (value !== 5) {
+          setValue(5)
+        }
+        break;
+      default:
+        break;
+    }
   }, [value]);
 
   return (
