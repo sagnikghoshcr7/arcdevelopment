886e942d9af8d97491b557dd1552d6f084cb5266
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-18 02:58:33 +0530

    Creating The Call To Action continued

--- src/components/ui/CallToAction.jsx
+++ src/components/ui/CallToAction.jsx
@@ -5,6 +5,9 @@ import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import ButtonArrow from './ButtonArrow';
 
+import background from '../../assets/background.jpg';
+import mobileBackground from '../../assets/mobileBackground.jpg';
+
 const useStyles = makeStyles(theme => ({
   learnButton: {
     ...theme.typography.learnButton,
@@ -15,29 +18,64 @@ const useStyles = makeStyles(theme => ({
       marginBottom: '2em'
     }
   },
+  background: {
+    backgroundImage: `url(${background})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '60em',
+    width: '100%',
+    [theme.breakpoints.down('md')]: {
+      backgroundImage: `url(${mobileBackground})`
+    }
+  },
+  estimateButton: {
+    ...theme.typography.estimate,
+    borderRadius: 50,
+    height: 80,
+    width: 205,
+    backgroundColor: theme.palette.common.orange,
+    fontSize: '1.5rem',
+    marginRight: '5em'
+  }
 }));
 
 export default function CallToAction() {
   const classes = useStyles();
   const theme = useTheme();
 
-  return <Grid container>
-    <Grid item>
-      <Grid container direction='column'>
-        <Grid item>
-          <Typography variant='h2'>Simple Software.<br />Revolutionary Results.</Typography>
-          <Typography variant='subtitle2'>Take advantage of the 21st Century.</Typography>
-          <Grid container item>
-            <Button
-              variant='outlined'
-              className={classes.learnButton}
+  return (
+    <Grid
+      container
+      alignItems='center'
+      justify='space-between'
+      className={classes.background}
+    >
+      <Grid item style={{marginLeft: '5em'}}>
+        <Grid container direction='column'>
+          <Grid item>
+            <Typography variant='h2'>Simple Software.<br />Revolutionary Results.</Typography>
+            <Typography
+              variant='subtitle2'
+              style={{fontSize: '1.5rem'}}
             >
-              <span style={{marginRight: 5}}>Learn More</span>
-              <ButtonArrow width={10} height={10} fill={theme.palette.common.blue} />
-            </Button>
+              Take advantage of the 21st Century.
+            </Typography>
+            <Grid container item>
+              <Button
+                variant='outlined'
+                className={classes.learnButton}
+              >
+                <span style={{marginRight: 5}}>Learn More</span>
+                <ButtonArrow width={10} height={10} fill={theme.palette.common.blue} />
+              </Button>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
+      <Grid item>
+        <Button variant='contained' className={classes.estimateButton}>Free Estimate</Button>
+      </Grid>
     </Grid>
-  </Grid>
+  );
 }
