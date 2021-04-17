4480e1eb493ddb505768a7f73e4135eacb8191fb
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-17 18:07:07 +0530

    create hero block

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Lottie from 'react-lottie';
-import {makeStyles} from '@material-ui/core/styles';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import ButtonArrow from '../components/ui/ButtonArrow';
 import animationData from '../animations/landinganimation/data';
 
 const useStyles = makeStyles(theme => ({
@@ -20,5 +23,29 @@ export default function LandingPage() {
     }
   }
 
-  return <Lottie options={defaultOptions} height={'100%'} width={'100%'} />
+  return (
+    <Grid container direction='column'>
+      <Grid item>
+        <Grid container direction='row'>
+          <Grid item>
+            <div>Bringing West Coast Technology<br/>to the Midwest</div>
+            <Grid container>
+              <Grid item>
+                <Button variant='contained'>Free Estimate</Button>
+              </Grid>
+              <Grid item>
+                <Button variant='outlined'>
+                  Learn More
+                  <ButtonArrow width={15} height={15} fill='red' />
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Lottie options={defaultOptions} height={'100%'} width={'100%'} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
