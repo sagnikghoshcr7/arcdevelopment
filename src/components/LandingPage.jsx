c674fdfd685813764ecdbcee0a9cdb05cbf61f6c
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-17 18:37:07 +0530

    style hero block

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,13 +3,19 @@ import Lottie from 'react-lottie';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import ButtonArrow from '../components/ui/ButtonArrow';
 import animationData from '../animations/landinganimation/data';
 
 const useStyles = makeStyles(theme => ({
-
-}))
+  animation: {
+    maxWidth: '50em',
+    minWidth: '21em',
+    marginTop: '2em',
+    marginLeft: '10%'
+  }
+}));
 
 export default function LandingPage() {
   const classes = useStyles();
@@ -26,9 +32,13 @@ export default function LandingPage() {
   return (
     <Grid container direction='column'>
       <Grid item>
-        <Grid container direction='row'>
-          <Grid item>
-            <div>Bringing West Coast Technology<br/>to the Midwest</div>
+        <Grid container justify='flex-end' alignItems='center' direction='row'>
+          <Grid sm item>
+            <Typography variant='h2' align='center'>
+              Bringing West Coast Technology
+              <br />
+              to the Midwest
+            </Typography>
             <Grid container>
               <Grid item>
                 <Button variant='contained'>Free Estimate</Button>
@@ -41,8 +51,12 @@ export default function LandingPage() {
               </Grid>
             </Grid>
           </Grid>
-          <Grid item>
-            <Lottie options={defaultOptions} height={'100%'} width={'100%'} />
+          <Grid sm item className={classes.animation}>
+            <Lottie
+              options={defaultOptions}
+              height={'100%'}
+              width={'100%'}
+            />
           </Grid>
         </Grid>
       </Grid>
