e1afdefb9e32fb906fb140058901bed690e4e65e
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-19 03:25:34 +0530

    Custom Software Development Page - Animations Row 1

--- src/components/CustomSoftware.jsx
+++ src/components/CustomSoftware.jsx
@@ -12,6 +12,8 @@ import forwardArrow from '../assets/forwardArrow.svg';
 import lightbulb from '../assets/bulb.svg';
 import cash from '../assets/cash.svg';
 import stopwatch from '../assets/stopwatch.svg';
+import documentsAnimation from '../animations/documentsAnimation/data';
+import scaleAnimation from '../animations/scaleAnimation/data.json';
 
 const useStyles = makeStyles(theme => ({
   heading: {
@@ -25,11 +27,32 @@ const useStyles = makeStyles(theme => ({
     paddingRight: '5em',
     paddingTop: '2em',
     paddingBottom: '10em'
+  },
+  itemContainer: {
+    maxWidth: '40em'
   }
 }));
 
 export default function CustomSoftware(props) {
   const classes = useStyles()
+
+  const documentsOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: documentsAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
+  const scaleOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: scaleAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
 
   return (
     <Grid container direction='column' className={classes.mainContainer}>
@@ -102,6 +125,52 @@ export default function CustomSoftware(props) {
           </Grid>
           <Grid item>
             <img src={cash} alt="cash" />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item container direction='row'>
+        <Grid item container className={classes.itemContainer}>
+          <Grid item container direction='column' md>
+            <Grid item>
+              <Typography variant='h4'>Digital Documents & Data</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant='body1' paragraph>
+                Reduce Errors. Reduce Waste. Reduce Costs.
+              </Typography>
+              <Typography variant='body1' paragraph>
+                Billions are spent annually on the purchasing, printing, and distributing of paper. On top of the massive environmental impact this has, it causes harm to your bottom line as well.
+              </Typography>
+              <Typography variant='body1' paragraph>
+                By utilizing digital and documents you can remove these obsolete expenses, accelerate your communication, and help the earth.
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item md>
+            <Lottie
+              options={documentsOptions}
+              isStopped={false}
+              style={{maxHeight: 325, maxWidth: 275, minHeight: 275}}
+            />
+          </Grid>
+        </Grid>
+        <Grid item container className={classes.itemContainer}>
+          <Grid item md>
+            <Lottie
+              options={scaleOptions}
+              isStopped={false}
+              style={{maxHeight: 260, maxWidth: 280}}
+            />
+          </Grid>
+          <Grid item container direction='column' md>
+            <Grid item>
+              <Typography variant='h4' align='right'>Scale</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant='body1' align='right' paragraph>
+                Whether you're a large brand, just getting started, or taking off right now, our application architecture ensures pain-free growth and reliability.
+              </Typography>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
