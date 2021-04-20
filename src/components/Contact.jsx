657605aac15a65b87b26a77bf502d788d8162bbe
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-21 01:14:15 +0530

    Contact Information

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,8 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 
 import background from '../assets/background.jpg';
+import phoneIcon from '../assets/phone.svg';
+import emailIcon from '../assets/email.svg';
 
 const useStyles = makeStyles(theme => ({
   background: {
@@ -24,10 +26,44 @@ export default function Contact() {
 
   return (
     <Grid container direction='row'>
-      <Grid item container direction='column' lg={3}>
+      <Grid item container direction='column' justify='center' lg={3}>
         <Grid item>
           <Typography variant='h2' style={{lineHeight: 1}}>Contact Us</Typography>
           <Typography variant='body1' style={{color: theme.palette.common.blue}}>We're waiting.</Typography>
+        </Grid>
+        <Grid item container>
+          <Grid item>
+            <img
+              src={phoneIcon}
+              alt="phone"
+              style={{marginRight: '0.5em', verticalAlign: 'bottom'}}
+            />
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="body1"
+              style={{color: theme.palette.common.blue, fontSize: '1rem'}}
+            >
+              (555) 555-5555
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item container>
+          <Grid item>
+            <img
+              src={emailIcon}
+              alt="envelope"
+              style={{marginRight: '0.5em', verticalAlign: 'bottom'}}
+            />
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="body1"
+              style={{color: theme.palette.common.blue, fontSize: '1rem'}}
+            >
+              sagnikghoshmandra@gmail.com
+            </Typography>
+          </Grid>
         </Grid>
       </Grid>
       <Grid item container className={classes.background} lg={9}></Grid>
