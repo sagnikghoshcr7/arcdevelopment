5c95e6e3efb9ac95ed8d19aab503ce556a8fa42e
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-21 02:30:23 +0530

    Using TextFields - State Controlled Values

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import {makeStyles, useTheme} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+
+import ButtonArrow from './ui/ButtonArrow';
 
 import background from '../assets/background.jpg';
 import phoneIcon from '../assets/phone.svg';
 import emailIcon from '../assets/email.svg';
+import airplane from '../assets/send.svg';
 
 const useStyles = makeStyles(theme => ({
   background: {
@@ -18,11 +22,43 @@ const useStyles = makeStyles(theme => ({
     backgroundRepeat: 'no-repeat',
     height: '60em',
   },
+  estimateButton: {
+    ...theme.typography.estimate,
+    borderRadius: 50,
+    height: 80,
+    width: 205,
+    backgroundColor: theme.palette.common.orange,
+    fontSize: '1.5rem',
+    marginRight: '5em',
+    marginLeft: '2em',
+    "&:hover": {
+      backgroundColor: theme.palette.secondary.light
+    },
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: 0,
+      marginRight: 0
+    }
+  },
+  learnButton: {
+    ...theme.typography.learnButton,
+    fontSize: '0.7rem',
+    height: 35,
+    padding: 5,
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: '2em'
+    }
+  }
 }));
 
-export default function Contact() {
+export default function Contact(props) {
   const classes = useStyles();
   const theme = useTheme();
+  const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
 
   return (
     <Grid container direction='row'>
@@ -65,8 +101,92 @@ export default function Contact() {
             </Typography>
           </Grid>
         </Grid>
+        <Grid item container>
+          <Grid item>
+            <TextField
+              label="Name"
+              id="name"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              label="Email"
+              id="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              label="Phone"
+              id="phone"
+              value={phone}
+              onChange={(event) => setPhone(event.target.value)}
+            />
+          </Grid>
+        </Grid>
+        <Grid item>
+          <TextField
+            value={message}
+            multiline
+            rows={10}
+            id='message'
+            onChange={event => setMessage(event.target.value)}
+          />
+        </Grid>
+        <Grid item>
+          <Button variant='contained'>
+            Send Message
+            <img src={airplane} alt="paper airplane" />
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item container className={classes.background} lg={9}></Grid>
+      <Grid item container className={classes.background} alignItems='center' lg={9}>
+        <Grid
+          item
+          style={{
+            marginLeft: matchesSM ? 0 : '5em',
+            textAlign: matchesSM ? 'center' : 'inherit'
+          }}
+        >
+          <Grid container direction='column'>
+            <Grid item>
+              <Typography variant='h2'>Simple Software.<br />Revolutionary Results.</Typography>
+              <Typography
+                variant='subtitle2'
+                style={{fontSize: '1.5rem'}}
+              >
+                Take advantage of the 21st Century.
+              </Typography>
+              <Grid container justify={matchesSM ? 'center' : undefined} item>
+                <Button
+                  component={Link}
+                  to='/revolution'
+                  variant='outlined'
+                  className={classes.learnButton}
+                  onClick={() => props.setValue(2)}
+                >
+                  <span style={{marginRight: 5}}>Learn More</span>
+                  <ButtonArrow width={10} height={10} fill={theme.palette.common.blue} />
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Button
+            component={Link}
+            to='/estimate'
+            variant='contained'
+            className={classes.estimateButton}
+            onClick={() => props.setValue(5)}
+          >
+            Free Estimate
+          </Button>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
