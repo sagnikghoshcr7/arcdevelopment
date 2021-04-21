74bb09bd54a9c8051ed8fb76dcf91a18fae10331
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-21 18:22:43 +0530

    Using TextFields - Responsive Design

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,6 +10,7 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 import ButtonArrow from './ui/ButtonArrow';
 
 import background from '../assets/background.jpg';
+import mobileBackground from '../assets/mobileBackground.jpg';
 import phoneIcon from '../assets/phone.svg';
 import emailIcon from '../assets/email.svg';
 import airplane from '../assets/send.svg';
@@ -21,7 +22,10 @@ const useStyles = makeStyles(theme => ({
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     height: '60em',
-    paddingBottom: '10em'
+    paddingBottom: '10em',
+    [theme.breakpoints.down('md')]: {
+      backgroundImage: `url(${mobileBackground})`
+    }
   },
   estimateButton: {
     ...theme.typography.estimate,
@@ -35,7 +39,7 @@ const useStyles = makeStyles(theme => ({
     "&:hover": {
       backgroundColor: theme.palette.secondary.light
     },
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down('md')]: {
       marginLeft: 0,
       marginRight: 0
     }
@@ -45,7 +49,7 @@ const useStyles = makeStyles(theme => ({
     fontSize: '0.7rem',
     height: 35,
     padding: 5,
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down('md')]: {
       marginBottom: '2em'
     }
   },
@@ -70,6 +74,7 @@ const useStyles = makeStyles(theme => ({
 export default function Contact(props) {
   const classes = useStyles();
   const theme = useTheme();
+  const matchesMD = useMediaQuery(theme.breakpoints.down('md'));
   const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
 
   const [name, setName] = useState("");
@@ -85,14 +90,30 @@ export default function Contact(props) {
         direction='column'
         justify='center'
         alignItems='center'
+        style={{
+          marginBottom: matchesMD ? '5em' : 0,
+          marginTop: matchesSM ? '1em' : matchesMD ? '5em' : 0
+        }}
         lg={4}
         xl={3}
       >
         <Grid item>
           <Grid container direction='column'>
             <Grid item>
-              <Typography variant='h2' style={{lineHeight: 1}}>Contact Us</Typography>
-              <Typography variant='body1' style={{color: theme.palette.common.blue}}>We're waiting.</Typography>
+              <Typography
+                align={matchesMD ? 'center' : undefined}
+                variant='h2'
+                style={{lineHeight: 1}}
+              >
+                Contact Us
+              </Typography>
+              <Typography
+                align={matchesMD ? 'center' : undefined}
+                variant='body1'
+                style={{color: theme.palette.common.blue}}
+              >
+                We're waiting.
+              </Typography>
             </Grid>
             <Grid item container style={{marginTop: '2em'}}>
               <Grid item>
@@ -107,7 +128,7 @@ export default function Contact(props) {
                   variant="body1"
                   style={{color: theme.palette.common.blue, fontSize: '1rem'}}
                 >
-                  (555) 555-5555
+                  (+91) 7044941540
                 </Typography>
               </Grid>
             </Grid>
@@ -128,27 +149,30 @@ export default function Contact(props) {
                 </Typography>
               </Grid>
             </Grid>
-            <Grid item container style={{maxWidth: '20em'}}>
-              <Grid item>
+            <Grid item container direction='column' style={{maxWidth: '20em'}}>
+              <Grid item style={{marginBottom: '0.5em'}}>
                 <TextField
                   label="Name"
                   id="name"
+                  fullWidth
                   value={name}
                   onChange={(event) => setName(event.target.value)}
                 />
               </Grid>
-              <Grid item>
+              <Grid item style={{marginBottom: '0.5em'}}>
                 <TextField
                   label="Email"
                   id="email"
+                  fullWidth
                   value={email}
                   onChange={(event) => setEmail(event.target.value)}
                 />
               </Grid>
-              <Grid item>
+              <Grid item style={{marginBottom: '0.5em'}}>
                 <TextField
                   label="Phone"
                   id="phone"
+                  fullWidth
                   value={phone}
                   onChange={(event) => setPhone(event.target.value)}
                 />
@@ -160,6 +184,7 @@ export default function Contact(props) {
                 value={message}
                 className={classes.message}
                 multiline
+                fullWidth
                 rows={10}
                 id='message'
                 onChange={event => setMessage(event.target.value)}
@@ -181,28 +206,35 @@ export default function Contact(props) {
       <Grid
         item
         container
+        direction={matchesMD ? 'column' : 'row'}
         className={classes.background}
         alignItems='center'
+        justify={matchesMD ? 'center' : undefined}
         lg={8}
         xl={9}
       >
         <Grid
           item
           style={{
-            marginLeft: matchesSM ? 0 : '3em',
-            textAlign: matchesSM ? 'center' : 'inherit'
+            marginLeft: matchesMD ? 0 : '3em',
+            textAlign: matchesMD ? 'center' : 'inherit'
           }}
         >
           <Grid container direction='column'>
             <Grid item>
-              <Typography variant='h2'>Simple Software.<br />Revolutionary Results.</Typography>
+              <Typography align={matchesMD ? 'center' : undefined} variant='h2'>
+                Simple Software.
+                <br />
+                Revolutionary Results.
+              </Typography>
               <Typography
+                align={matchesMD ? 'center' : undefined}
                 variant='subtitle2'
                 style={{fontSize: '1.5rem'}}
               >
                 Take advantage of the 21st Century.
               </Typography>
-              <Grid container justify={matchesSM ? 'center' : undefined} item>
+              <Grid container justify={matchesMD ? 'center' : undefined} item>
                 <Button
                   component={Link}
                   to='/revolution'
