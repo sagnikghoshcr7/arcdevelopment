ab7acfafa73f53c8be08dd215f45a666ba6deb56
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-21 19:45:35 +0530

    Using Dialogs - Setup

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,8 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
 
 import ButtonArrow from './ui/ButtonArrow';
 
@@ -86,6 +88,8 @@ export default function Contact(props) {
   const [phoneHelper, setPhoneHelper] = useState("");
 
   const [message, setMessage] = useState("");
+
+  const [open, setOpen] = useState(false);
 
   const onChange = event => {
     let valid;
@@ -246,6 +250,7 @@ export default function Contact(props) {
                 }
                 variant='contained'
                 className={classes.sendButton}
+                onClick={() => setOpen(true)}
               >
                 Send Message
                 <img
@@ -258,6 +263,89 @@ export default function Contact(props) {
           </Grid>
         </Grid>
       </Grid>
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogContent>
+          <Grid container direction='column'>
+            <Grid item>
+              <Typography variant='h4' gutterBottom>
+                Confirm Message
+              </Typography>
+            </Grid>
+            <Grid item style={{marginBottom: '0.5em'}}>
+              <TextField
+                label="Name"
+                id="name"
+                fullWidth
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+              />
+            </Grid>
+            <Grid item style={{marginBottom: '0.5em'}}>
+              <TextField
+                label="Email"
+                error={emailHelper.length !== 0}
+                helperText={emailHelper}
+                id="email"
+                fullWidth
+                value={email}
+                onChange={onChange}
+              />
+            </Grid>
+            <Grid item style={{marginBottom: '0.5em'}}>
+              <TextField
+                label="Phone"
+                error={phoneHelper.length !== 0}
+                helperText={phoneHelper}
+                id="phone"
+                fullWidth
+                value={phone}
+                onChange={onChange}
+              />
+            </Grid>
+          </Grid>
+          <Grid item style={{maxWidth: '20em'}}>
+            <TextField
+              InputProps={{disableUnderline: true}}
+              value={message}
+              className={classes.message}
+              multiline
+              fullWidth
+              rows={10}
+              id='message'
+              onChange={event => setMessage(event.target.value)}
+            />
+          </Grid>
+          <Grid item container>
+            <Grid item>
+              <Button color='primary' onClick={() => setOpen(false)}>
+                Cancel
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                disabled={
+                  name.length === 0 ||
+                  message.length === 0 ||
+                  phone.length === 0 ||
+                  email.length === 0 ||
+                  phoneHelper.length !== 0 ||
+                  emailHelper.length !== 0
+                }
+                variant='contained'
+                className={classes.sendButton}
+                onClick={() => setOpen(true)}
+              >
+                Send Message
+                <img
+                  src={airplane}
+                  alt="paper airplane"
+                  style={{marginLeft: '1em'}}
+                />
+              </Button>
+            </Grid>
+          </Grid>
+        </DialogContent>
+      </Dialog>
       <Grid
         item
         container
